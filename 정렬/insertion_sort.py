daa2bc3a2bb448950e5f05f1ce26886767a5498d
wnsdud1302 <63913862+wnsdud1302@users.noreply.github.com>
2020-04-19 23:22:51 +0900

    Update insertion_sort.py

--- 정렬/insertion_sort.py
+++ 정렬/insertion_sort.py
@@ -2,8 +2,42 @@ import random
 
 a = [random.randint(1,100) for i in range(10)]
 b = a.copy()
-def insertion_sort(list):
+print("list a : ", a)
+print("list b : ", b)
+def insertion_sort1(list):
     length = len(list)
     for i in range(length):
         tmp = list[i]
-        for j in range(
+        j = i
+        while j > 0 and list[j-1] > tmp:
+            list[j] = list[j-1]
+        list[j] = tmp
+
+def insertion_sort2(list):
+	length = len(list)
+	for i in range(length):
+		key = list[i]
+		pl = int()
+		pr = i - 1
+		pc = int()
+		pd = int()
+		while True:
+			pc = (pl+pr) // 2
+			if list[pc] == key:
+				return pc
+			elif list[pc] < key:
+				pl = pc + 1
+			elif list[pc] > key:
+				pr = pc - 1
+			if pl <= pr: continue
+			break
+		if pl <= pr : pd = pc + 1
+		else : pd = pr + 1
+		for j in range(i , pd, -1):
+			list[j] = list[j-1]
+		list[pd] = key
+		
+insertion_sort1(a)
+insertion_sort2(b)
+print("정렬된 리스트 : ", a)
+print("정렬된 리스트 : ", b)
