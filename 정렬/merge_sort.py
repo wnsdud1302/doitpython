4fe6045f73c631ed1abb7d00435bbc9cb9b505d4
wnsdud1302 <63913862+wnsdud1302@users.noreply.github.com>
2020-04-22 23:09:40 +0900

    Update merge_sort.py
    새롭게 다시짬

--- 정렬/merge_sort.py
+++ 정렬/merge_sort.py
@@ -1,32 +1,48 @@
 import random
-def merge_sort(list1, list2,list3):
-    i = int()
-    j = int()
-    while i < len(list1) and j < len(list2):
-        if list1[i] > list2[j]:
-            list3.append(list2[j])
-            j = j + 1
-        elif list1[i] < list2[j]:
-            list3.append(list1[i])
-            i = i + 1
-        else :
-            list3.append(list1[i])
-            i = i + 1
-            j = j + 1
-    while i < len(list1):
-        list3.append(list1[i])
-        i = i + 1
-    while j < len(list2):
-        list3.append(list2[j])
-        j = j + 1
+def __merge_sort(list1, list2,list3):
+    while True:
+        if list1 == None:
+            while list2 != None:
+                list3.append(list2.pop(0))
+            break
+        if list2 == None:
+            while list1 != None:
+                list3.append(list1.pop(0))
+            break
+        if list1[0] > list2[0]:
+            list3.append(list2.pop(0))
+            continue
+        if list1[0] < list2[0]:
+            list3.append(list1.pop(0))
+            continue
+        if list1[0] == list2[0]:
+            list3.append(list1.pop(0))
+            list3.append(list2.pop(0))
+            continue
+        
+
+
+def merge_sort(exlist, sort_list):
+    length = len(exlist)
+    left_list = list()
+    right_list = list()
+    left = 0
+    center = length//2
+    right = center
+    while left < center:
+        left_list.append(exlist[left])
+        left_list.sort()
+        left = left + 1
+    while right < length:
+        right_list.append(exlist[right])
+        right_list.sort()
+        right = right + 1
+    __merge_sort(left_list,right_list,sort_list)
+
+
+
 
 a = list(random.randint(1,50) for i in range(0,10))
-b = list(random.randint(1,50) for i in range(0,10))
-c = list()
-a.sort()
-b.sort()
 print("리스트 a : ",a)
-print("리스트 b : ",b)
-merge_sort(a,b,c)
-print("리스트 c ", c)
-print(len(c))
+merge_sort(a, a)
+print("정렬된 리스트 a : ", a)
