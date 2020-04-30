3ea68c3ec22ebae4bf09540424b7e395fefa25db
wnsdud1302 <63913862+wnsdud1302@users.noreply.github.com>
2020-04-30 23:02:34 +0900

    Update double_linked_list.py

--- 자료구조/double_linked_list.py
+++ 자료구조/double_linked_list.py
@@ -6,29 +6,45 @@ class Node:
 
 class double_linked_list:
     def __init__(self):
+        tmpNode = Node(None)
         self.head = Node("head")
-        self.head.prev = self.head.next = self.head
-    def insertFront(self, data):
-        insert(data, self.head)
-
+        self.head.next = tmpNode
+        tmpNode.next = tmpNode.prev = tmpNode
     
+    def insertFront(self, data):
+        insert(data, self.head.next)
+
     def insertRear(self, data):
         insert(data,self.head.next.prev)
-
     
+    def removeFront(self):
+        remove(self.head.next.next)
+
+    def removeRear(self):
+        remove(self.head.next.prev)
+
     def print(self):
         start = self.head.next
         if start.next == None:
             print("비어있습니다!")
-        while start != self.head:
+        while True:
             print(str(start.data), end=" ")
             start = start.next
             if start:
                 print("-->", end=" ")
+            if start.data == None: break
+            continue
         print()
+
 def insert(data, p):
-    new_node = Node(data, p.next, p)
-    p.next = p.next.prev = new_node
+    nxt = p.next
+    new_node = Node(data, nxt, p)
+    p.next = nxt.prev = new_node
+
+def remove(p):
+    p.prev.next = p.next
+    p.next.prev = p.prev
+    del p
 
     
 
@@ -39,4 +55,7 @@ mylist.insertRear(5)
 mylist.insertFront(3)
 mylist.insertFront(4)
 mylist.insertFront(7)
+mylist.insertRear(2)
+mylist.removeFront()
+mylist.removeRear()
 mylist.print()
