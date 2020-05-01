5ad821a985517221769d0ae5ccca250d270f0c1b
wnsdud1302 <63913862+wnsdud1302@users.noreply.github.com>
2020-05-01 23:45:20 +0900

    Update Tree.py

--- 자료구조/Tree.py
+++ 자료구조/Tree.py
@@ -1,3 +1,4 @@
+ 
 class BinNode:
     def __init__(self, data, left = None, right = None):
         self.data = data
@@ -8,11 +9,62 @@ class BinNode:
         print(self.data)
 
 class Tree:
-    def __init__(self):
-        self.root = None
-    
-    def printTree(self):
-        if self.root != None:
-            printTree(self.root.left)
-            self.root.printBinNode()
-            printTree(self.root.right)
+    def __init__(self, root):
+        self.root = BinNode(root)
+
+    def add(self, data):
+        start = self.root
+        while True:
+            if start.data > data:
+                if start.left != None:
+                    start = start.left
+                else:
+                    start.left = BinNode(data)
+                    break
+            else:
+                if start.right != None:
+                    start = start.right
+                else:
+                    start.right = BinNode(data)
+                    break
+
+    def remove(self, data):
+        start = self.root
+        parent = start
+        while start:
+            if start.data == data:
+                break
+            elif start.data > data:
+                parent = start
+                start = start.left
+            elif start.data < data:
+                parent = start
+                start = start.right
+            else:
+                return False
+        if start.left == None and start.right == None:
+            if parent.data > data:
+                parent.left = None
+            else:
+                self.parent.right = None
+        elif start.left != None and start.right == None:
+            if parent.data > data:
+                parent.left = start.left
+            else:
+                parent.right = start.left
+
+
+    def search(self, data):
+        start = self.root
+        while start:
+            if start.data == data:
+                print(start.data)
+                return True
+            elif start.data < data:
+                print(start.data)
+                start = start.left
+            else:
+                print(start.data)
+                start = start.right
+            
+
