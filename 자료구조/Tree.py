d9c88b67832264131920f9fc3d4a9b5940d0d6cc
wnsdud1302 <63913862+wnsdud1302@users.noreply.github.com>
2020-05-02 23:15:59 +0900

    Update Tree.py

--- 자료구조/Tree.py
+++ 자료구조/Tree.py
@@ -1,4 +1,3 @@
- 
 class BinNode:
     def __init__(self, data, left = None, right = None):
         self.data = data
@@ -8,7 +7,7 @@ class BinNode:
     def printBinNode(self):
         print(self.data)
 
-class Tree:
+class BinTree:
     def __init__(self, root):
         self.root = BinNode(root)
 
@@ -29,37 +28,45 @@ class Tree:
                     break
 
     def remove(self, data):
-        start = self.root
-        parent = start
-        while start:
-            if start.data == data:
+        del_node = self.root
+        parent_node = del_node
+        while True:
+            if del_node.data == data:
                 break
-            elif start.data > data:
-                parent = start
-                start = start.left
-            elif start.data < data:
-                parent = start
-                start = start.right
+            elif del_node.data > data:
+                parent_node = del_node
+                del_node = del_node.left
+            elif del_node.data < data:
+                parent_node = del_node
+                del_node = del_node.right
+        
+        if del_node.left == None and del_node.right == None:
+            if parent_node.data > data:
+                parent_node.left = None
             else:
-                return False
-        if start.left == None and start.right == None:
-            if parent.data > data:
-                parent.left = None
+                parent_node.right = None
+        elif del_node.left != None and del_node.right == None:
+            if parent_node.data > data:
+                parent_node.left = del_node.left
             else:
-                self.parent.right = None
-        elif start.left != None and start.right == None:
-            if parent.data > data:
-                parent.left = start.left
+                parent_node.right = del_node.left
+        elif del_node.left == None and del_node.right != None:
+            if parent_node.data > data:
+                parent_node.left = del_node.right
             else:
-                parent.right = start.left
-        elif start.left == None and start.right != None:
-            if parent.data > data:
-                parent.left = start.right
-            else:
-                parent.right = start.left
-        elif start.left != None and start.right != None:
-            pass
-
+                parent_node.right = del_node.right
+        else:
+            change_node = del_node
+            parent_node = del_node
+            del_node = del_node.right
+            while del_node.left:
+                parent_node = del_node
+                del_node = del_node.left
+            change_node.data = del_node.data
+            if del_node.right != None:
+                parent_node.left = del_node.right
+            else :
+                parent_node.left = None
 
     def search(self, data):
         start = self.root
@@ -67,11 +74,9 @@ class Tree:
             if start.data == data:
                 print(start.data)
                 return True
-            elif start.data < data:
+            elif start.data > data:
                 print(start.data)
                 start = start.left
             else:
                 print(start.data)
                 start = start.right
-            
-
